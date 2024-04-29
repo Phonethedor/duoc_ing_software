fb131e6c484a7b4c215a93c67aacbb7faf308673
Alvaro Giovanni Castillo Muñoz <alvaroc.giovannim@gmail.com>
2024-04-29 19:53:19 -0400

    agregado proyecto inicial

---
+++ rentApart/rentApart/start/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+    tipo = models.IntegerField(default=2)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellido
+    
+class CategoriaHabitacion(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class Habitacion(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+    precio = models.FloatField()
+    disponible = models.BooleanField()
+    categoria = models.ForeignKey(CategoriaHabitacion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+    
+class MetodoPago(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class Reserva(models.Model):  
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    habitacion = models.ForeignKey(Habitacion, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    metodo_pago = models.ForeignKey(MetodoPago, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.usuario.nombre + ' ' + self.usuario.apellido + ' ' + self.habitacion.nombre
+    
+class Informe(models.Model):
+    fecha = models.DateField()
+    descripcion = models.TextField()
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.usuario.nombre + ' ' + self.usuario.apellido
