ceec3a822ea5b79f889179386e6378966dfa2a42
Alvaro Giovanni Castillo Muñoz <alvaroc.giovannim@gmail.com>
2024-04-29 21:24:19 -0400

    base

--- rentApart/rentApart/start/views.py
+++ rentApart/rentApart/start/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .modelmodels import *
 
 # Create your views here.
+def start(request):
+    user_id= request.session.get('id_usuario')
+
+    if user_id  is not None:
+
+        usuario=Usuario.objects.get(id_usuario=user_id) 
+        if usuario:
+            context = {
+                "usuario": usuario,
+            }
+            return render(request, 'inicio/index.html', context)
+       
+    return render(request, 'index.html')
+
+def reserva(request):
+    user_id= request.session.get('id_usuario')
+
+    if user_id  is not None:
+
+        usuario = Usuario.objects.get(id_usuario=user_id) 
+        if usuario:
+            context = {
+                "usuario": usuario,
+            }
+            return render(request, 'reserva/index.html', context)
+       
+    return render(request, 'index.html')
+
+def registro(request):
+    return render(request, 'registro/index.html')
