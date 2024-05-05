1efb2c25b36882eb7a86edbd36c5761a03c3fc4e
Alvaro Giovanni Castillo Muñoz <alvaroc.giovannim@gmail.com>
2024-05-05 01:31:37 -0400

    etiquetas front, listas

--- rentApart/rentApart/start/urls.py
+++ rentApart/rentApart/start/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import *
 
+'''
+    TODO:
+    - agregar las urls restantes, luego de haber creado las vistas
+'''
+
 urlpatterns = [
-    path('', start, name='start'),
-    path('reserva', reserva, name='reserva'),
+    path('', start, name='index'),
+    path('reserva/', reserva, name='reserva'),
+    path('registro/', registro, name='registro'),
+    path('ingresar/', ingresar, name='ingresar'),
 ]
--- rentApart/rentApart/start/views.py
+++ rentApart/rentApart/start/views.py
@@ -1,19 +1,17 @@
-from django.shortcuts import render
-from .modelmodels import *
+from django.shortcuts import render, redirect
+from .models import *
 
-# Create your views here.
 def start(request):
     user_id= request.session.get('id_usuario')
 
     if user_id  is not None:
 
-        usuario=Usuario.objects.get(id_usuario=user_id) 
+        usuario = Usuario.objects.get(id_usuario=user_id) 
         if usuario:
             context = {
                 "usuario": usuario,
             }
             return render(request, 'inicio/index.html', context)
-       
     return render(request, 'index.html')
 
 def reserva(request):
@@ -26,9 +24,54 @@ def reserva(request):
             context = {
                 "usuario": usuario,
             }
-            return render(request, 'reserva/index.html', context)
-       
-    return render(request, 'index.html')
+            return render(request, 'index.html', context)
+    return render(request, 'reserva.html')
 
 def registro(request):
-    return render(request, 'registro/index.html')
+    return render(request, 'registro.html')
+
+'''
+    prototipo de registro, sin testear
+'''
+def register(request):
+    correo = request.POST['email']
+    nombre = request.POST['nombre']
+    apellido = request.POST['apellido']
+    password = request.POST['password']
+
+    Usuario.objects.create(email=correo, nombre=nombre, apellido=apellido,pass_usuario = password, tipo=2)
+    return redirect('start')
+
+def ingresar(request):
+    return render(request, 'login.html')
+
+'''
+    prototipo de login, sin testear
+'''
+def  login(request):
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+
+    try:
+        user = Usuario.objects.get(email=email)
+    except Usuario.DoesNotExist:
+        return redirect('ingresar')
+
+    if user.password == password:
+        request.session['id_usuario'] = user.id_usuario
+        return redirect('start')
+    else:
+        return redirect('ingresar')
+    
+'''
+    TODO:
+    - testear login
+    - testear registro
+    - implementar logout
+    - implementar registro de reservas (comun)
+    - implementar registro de habitaciones (admin)
+    - implementar modificacion de usuario (comun)
+    - implementar modificacion de usuario (admin)
+    - implementar modificacion de habitaciones (admin)
+    - implementar vista de historial de reservas (admin)
+'''
